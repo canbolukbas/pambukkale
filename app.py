5c743ec3d2ab8873eaaf0bd2b353d69f536da017
can <canbolukbas98@gmail.com>
2026-05-10 15:55:44 +0300

    remove list endpoint for now

--- app.py
+++ app.py
@@ -1,19 +1,3 @@
-import uuid
-from datetime import datetime
-
 from flask import Flask
 
 app = Flask(__name__)
-
-
-@app.get("/")
-def list_trips():
-    trips = [
-        {
-            "id": uuid.uuid4(),
-            "name": "",
-            "departure": datetime.now(),
-            "price": 0,
-        }
-    ]
-    return {"trips": [trip for trip in trips]}
